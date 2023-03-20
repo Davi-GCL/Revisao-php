ca451288ad2f02655c3ad6edeb6585c04f64e6af
Davi-GCL <1davi.gabriel1@gmail.com>
2023-03-20 20:51:03 -0300

    add intermediario e texto

---
+++ intermediario.php
@@ -0,0 +1,29 @@
+<?php
+//Topico 1:
+    // $conteudo = file_get_contents("texto.txt");
+
+    // echo $conteudo;
+
+//Topico 2:
+    // $num = 5;
+    // $fatorial = $num;
+    
+    // for($i = $num-1; $i > 0;$i--){
+    //     $fatorial *= $i;
+    // }
+
+    // echo $fatorial;
+
+//Topico 3:
+    // $email = "luanS@ntana@gmail.com.br";
+
+    // if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+    //     echo "$email é valido!";
+    // }
+    // else{
+    //     echo "$email NAO é valido";
+    // }
+
+//Topico 4:
+        
+?>
