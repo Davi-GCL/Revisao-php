131a69538edec7c86da639f24a92b5fd92d8d5ac
Davi-GCL <1davi.gabriel1@gmail.com>
2023-03-20 18:55:03 -0300

    commit inicial

---
+++ basico.php
@@ -0,0 +1,34 @@
+<?php
+    //TOpico 1:
+        echo "Hello World! <br>";
+
+    //Topico 2:
+        // $n1 = 5;
+        // $n2 = 15;
+        // $soma = $n1 + $n2;
+
+        // echo "$n1 + $n2 = $soma";
+
+    //Topico 3:
+        // $num = 10;
+    
+        // if($num % 2 == 0){
+        //     echo "$num é par!";
+        // }
+        // else{
+        //     echo "$num é impar";           
+        // }
+
+    //Topico 4:
+        for($i = 1;$i <= 10; $i++){
+            echo "$i, ";
+        }
+    
+    //Topico 5:
+        $n1 = 4;
+        $n2 = 2;
+        $n3 = 8;
+
+        $media = ($n1 + $n2 + $n3)/3;
+        echo $media; 
+?>
